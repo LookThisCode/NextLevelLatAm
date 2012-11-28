4e9cac92795374e4e93b45c0dd129ff263ddd078
Matias Moran Losada <mmoran@mmoranhost.(none)>
2012-11-28 18:36:59 -0300

    Grupo 3 desafio android version inicial

---
+++ Desafios/grupo3-Android/src/com/macacoders/antibulling/ServerUtilities.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.macacoders.antibulling;
+
+import static com.macacoders.antibulling.CommonUtilities.SERVER_URL;
+import static com.macacoders.antibulling.CommonUtilities.TAG;
+import static com.macacoders.antibulling.CommonUtilities.displayMessage;
+
+import com.google.android.gcm.GCMRegistrar;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
+/**
+ * Helper class used to communicate with the demo server.
+ */
+public final class ServerUtilities {
+
+    private static final int MAX_ATTEMPTS = 1;
+    private static final int BACKOFF_MILLI_SECONDS = 2000;
+    private static final Random random = new Random();
+
+    /**
+     * Register this account/device pair within the server.
+     *
+     * @return whether the registration succeeded or not.
+     */
+    static boolean register(final Context context, final String regId) {
+        Log.i(TAG, "registering device (regId = " + regId + ")");
+        String serverUrl = SERVER_URL;
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("regId", regId);
+        long backoff = BACKOFF_MILLI_SECONDS + random.nextInt(1000);
+        return false;
+    }
+
+    /**
+     * Unregister this account/device pair within the server.
+     */
+    static void unregister(final Context context, final String regId) {
+        Log.i(TAG, "unregistering device (regId = " + regId + ")");
+        String serverUrl = SERVER_URL;
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("regId", regId);
+        try {
+            post(serverUrl, params);
+            GCMRegistrar.setRegisteredOnServer(context, false);
+            String message = context.getString(R.string.server_unregistered);
+            CommonUtilities.displayMessage(context, message);
+        } catch (IOException e) {
+            // At this point the device is unregistered from GCM, but still
+            // registered in the server.
+            // We could try to unregister again, but it is not necessary:
+            // if the server tries to send a message to the device, it will get
+            // a "NotRegistered" error message and should unregister the device.
+            String message = context.getString(R.string.server_unregister_error,
+                    e.getMessage());
+            CommonUtilities.displayMessage(context, message);
+        }
+    }
+
+    /**
+     * Issue a POST request to the server.
+     *
+     * @param endpoint POST address.
+     * @param params request parameters.
+     *
+     * @throws IOException propagated from POST.
+     */
+    private static void post(String endpoint, Map<String, String> params)
+            throws IOException {
+        URL url;
+        try {
+            url = new URL(endpoint);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("invalid url: " + endpoint);
+        }
+        StringBuilder bodyBuilder = new StringBuilder();
+        Iterator<Entry<String, String>> iterator = params.entrySet().iterator();
+        // constructs the POST body using the parameters
+        while (iterator.hasNext()) {
+            Entry<String, String> param = iterator.next();
+            bodyBuilder.append(param.getKey()).append('=')
+                    .append(param.getValue());
+            if (iterator.hasNext()) {
+                bodyBuilder.append('&');
+            }
+        }
+        String body = bodyBuilder.toString();
+        Log.v(TAG, "Posting '" + body + "' to " + url);
+        byte[] bytes = body.getBytes();
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setFixedLengthStreamingMode(bytes.length);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded;charset=UTF-8");
+            // post the request
+            OutputStream out = conn.getOutputStream();
+            out.write(bytes);
+            out.close();
+            // handle the response
+            int status = conn.getResponseCode();
+            if (status != 200) {
+              throw new IOException("Post failed with error code " + status);
+            }
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+      }
+}
